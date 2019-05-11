1d86749432ca93f4255e7f773c88172a36d5e172
Winnnnnnnn <winn_huang@163.com>
2019-05-12 00:02:46 +0800

    第一次提交项目代码

---
+++ app/src/main/java/com/main/faq/MainActivity.java
@@ -0,0 +1,117 @@
+package com.main.faq;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.hide();
+        }
+        mWebView = findViewById(R.id.webView);
+        //webView加载网页后出现ERR_UNKNOWN_URL_SCHEME
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if(url == null) return false;
+
+                try {
+                    if(url.startsWith("weixin://") //微信
+                            || url.startsWith("alipays://") //支付宝
+                            || url.startsWith("mailto://") //邮件
+                            || url.startsWith("tel://")//电话
+                            || url.startsWith("dianping://")//大众点评
+                            || url.startsWith("baiduboxapp://") //百度
+                        //其他自定义的scheme
+                    ) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(intent);
+                        return true;
+                    }
+                } catch (Exception e) { //防止crash (如果手机上没有安装处理某个scheme开头的url的APP, 会导致crash)
+                    return true;//没有安装该app时，返回true，表示拦截自定义链接，但不跳转，避免弹出上面的错误页面
+                }
+
+                //处理http和https开头的url
+                mWebView.loadUrl(url);
+                return true;
+            }
+        });
+        WebSettings webSettings = mWebView.getSettings();
+        // 让WebView能够执行javaScript
+        webSettings.setJavaScriptEnabled(true);
+        // 让JavaScript可以自动打开windows
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        // 设置缓存
+        webSettings.setAppCacheEnabled(false);
+        // 设置缓存模式,一共有四种模式
+        //webSettings.setCacheMode(webSettings.LOAD_CACHE_ELSE_NETWORK);
+        // 支持缩放(适配到当前屏幕)
+        webSettings.setSupportZoom(true);
+        // 将图片调整到合适的大小
+        webSettings.setUseWideViewPort(true);
+        // 支持内容重新布局,一共有四种方式
+        // 默认的是NARROW_COLUMNS
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        // 设置可以被显示的屏幕控制
+        webSettings.setDisplayZoomControls(true);
+        // 设置默认字体大小
+        webSettings.setDefaultFontSize(12);
+        //支持video
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
+        // 加载需要显示的网页
+        mWebView.loadUrl("http://106.15.185.137:69/");
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message,
+                                     JsResult result) {
+                // TODO Auto-generated method stub
+                return super.onJsAlert(view, url, message, result);
+            }
+        });
+    }
+
+    /**
+     * 返回按钮监听
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) ) {
+            if (mWebView.canGoBack())
+            {
+                mWebView.goBack(); //goBack()表示返回WebView的上一页面
+                return true;
+            }else
+            {
+                finish();
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
